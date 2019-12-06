ee111d863f272364d87e30591cb5ca15e0199655
chimpansiets <svoort1@live.nl>
2019-12-06 09:13:09 +0100

    Part1 finally completo!!!

---
+++ Day06/part1.py
@@ -0,0 +1,34 @@
+import networkx as nx
+
+def add_planet(graph, input):
+    inner = input[:input.find(')')]
+    outer = input[input.find(')') + 1:]
+    if (inner not in graph):
+        graph.add_node(inner)
+    if (outer not in graph):
+        graph.add_node(outer)
+    graph.add_edge(inner, outer)
+
+def get_indirect_orbits(graph, node, ctr):
+    if (len(list(graph.successors(node))) == 0):
+        return (0)
+    else:
+        node_list = list(graph.successors(node))
+        ctr += len(node_list)
+        for elem in node_list:
+            ctr += get_indirect_orbits(graph, elem, 0)
+        return (ctr)
+
+
+if __name__ == "__main__":
+    G = nx.DiGraph()
+
+    for _ in range(2306):
+        input_line = input()
+        add_planet(G, input_line)
+    nodes = G.nodes()
+    edges = G.edges()
+    indirect_orbits = 0
+    for node in nodes:
+        indirect_orbits += get_indirect_orbits(G, node, 0)
+    print(indirect_orbits)
